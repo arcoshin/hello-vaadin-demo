8eebbf74b4c58491614af00a7e1f7e55fe75c9b4
josh <arcoshin@gmail.com>
2026-08-19 14:35:44 +0800

    init

---
+++ src/main/java/com/tsri/hellovaadindemo/HelloView.java
@@ -0,0 +1,33 @@
+package com.tsri.hellovaadindemo;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class HelloView extends VerticalLayout {
+
+    public HelloView() {
+
+        H1 hello = new H1("HELLO!!");
+
+        TextField input = new TextField("請輸入文字");
+
+        input.setValueChangeMode(ValueChangeMode.EAGER);
+
+        input.addValueChangeListener(event -> {
+
+            String value = event.getValue();
+
+            if (value == null || value.trim().isEmpty()) {
+                hello.setText("HELLO!!");
+            } else {
+                hello.setText("HELLO " + value + "!!");
+            }
+        });
+
+        add(hello, input);
+    }
+}
